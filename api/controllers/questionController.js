44ea989507e6e616c1e4781c24d24fa1a185b894
sumukshashidhar <sumukuuu@gmail.com>
2020-08-16 00:37:00 +0530

    finished the creating of questions

--- api/controllers/questionController.js
+++ api/controllers/questionController.js
@@ -1,33 +1,52 @@
 const { nanoid } = require("nanoid");
 const question = require("./../models/question")
+const test = require("./../models/test")
+
+async function generateQuestionID() {
+    return new Promise(async (res, rej) => {
+        res(await nanoid(10));
+    })
+}
 module.exports = {
-    generateQuestionID: async () => {
-        return new Promise(async (res, rej) => {
-            res(await nanoid(10));
-        })
-    },
 
 
     createQuestion: async (questionText, questionOptions, questionAnswer, questionTest, questionImage,) => {
-        return new Promise(async (res, rej) => {
-            // first, let us generate a question id
-            const newQuestion = new question({
-                questionText:questionText,
-                questionOptions:questionOptions,
-                testID:questionTest,
-                questionAnswer:questionAnswer,
-                questionImage:questionImage
-            })
+        // we don't really need to wait for this to happen, and it can happen in the background, so why
+        // stop the user, let the user continue adding
+        // first, let us generate a question id
+        const newQuestion = new question({
+            questionText:questionText,
+            questionOptions:questionOptions,
+            testID:questionTest,
+            questionAnswer:questionAnswer,
+            questionImage:questionImage,
+            questionID: await generateQuestionID()
+        })
 
-            newQuestion.save((err, obj) => {
-                if(err) {
-                    console.error(err)
-                    rej(err)
-                }
-                else {
-                    res(true)
-                }
-            })
+        newQuestion.save((err, obj) => {
+            if(err) {
+                console.error(err)
+            }
+            else{
+                //means that this particular question was created, let us now
+                // add this question to the list of questions in the test
+                // we need to first get the object, and then update it
+                test.findOne({testID:questionTest}, {testQuestions: 1}, (err2, test_obj) => {
+                    if(err3) {
+                        console.error(err2)
+                    }
+                    else {
+                        console.log(obj) // need to check how this format is
+                        test_obj["testQuestions"].push(obj["questionID"])
+                        test.findOneAndUpdate({testID:questionTest}, {$set: {testQuestions:test_obj["testQuestions"]}}, (err3, final) => {
+                            if(err3) {
+                                console.log(err3)
+                            }
+                            // if it reaches here, all the updates are done and it all works like a charm
+                        })
+                    }
+                })
+            }
         })
     }
 }
